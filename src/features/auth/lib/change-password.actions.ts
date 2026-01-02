ce3606ab69b5df4f0ef3b58af981942b62e024a1
David Valdiviezo <hvaldiviezos@unprg.edu.pe>
2026-01-02 15:19:26 -0500

    Update change-password.actions.ts

--- src/features/auth/lib/change-password.actions.ts
+++ src/features/auth/lib/change-password.actions.ts
@@ -4,14 +4,11 @@ import type {
   ChangePasswordResponse,
 } from "./change-password.interface";
 
-const BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost/milla-backend/public/api";
-
 export const changePassword = async (
   data: ChangePasswordRequest
 ): Promise<ChangePasswordResponse> => {
   const response = await api.post<ChangePasswordResponse>(
-    `${BASE_URL}/change-password`,
+    `change-password`,
     data
   );
   return response.data;
