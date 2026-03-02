98a37edc8050e5557fa3cb28636aad5f199661a0
David Valdiviezo <hvaldiviezos@unprg.edu.pe>
2026-03-02 12:57:23 -0500

    Update QuotationSection.tsx

--- src/features/ap/facturacion/electronic-documents/components/sections/QuotationSection.tsx
+++ src/features/ap/facturacion/electronic-documents/components/sections/QuotationSection.tsx
@@ -35,7 +35,7 @@ export function QuotationSection({ form }: QuotationSectionProps) {
         additionalParams={{
           is_approved: 1, // Solo cotizaciones aprobadas
           is_paid: 0, // Solo cotizaciones no pagadas
-          has_vehicle: 1, // Solo cotizaciones que tienen vehículo asociado
+          // has_vehicle: 1, // Solo cotizaciones que tienen vehículo asociado
         }}
         useFindByIdHook={usePurchaseRequestQuoteById}
       />
