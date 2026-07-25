62009f55d83375052a95e1406b8f68d175e8084d
david3010M <hvaldiviezos@unprg.edu.pe>
2026-07-25 13:09:31 -0500

    Update PurchaseRequestQuoteDetailModal.tsx

--- src/features/ap/comercial/solicitudes-cotizaciones/components/PurchaseRequestQuoteDetailModal.tsx
+++ src/features/ap/comercial/solicitudes-cotizaciones/components/PurchaseRequestQuoteDetailModal.tsx
@@ -218,7 +218,7 @@ export default function PurchaseRequestQuoteDetailModal({
       }
       icon="FileText"
       side="right"
-      size="5xl"
+      size="4xl"
       isLoading={isLoading}
     >
       {quote && (
