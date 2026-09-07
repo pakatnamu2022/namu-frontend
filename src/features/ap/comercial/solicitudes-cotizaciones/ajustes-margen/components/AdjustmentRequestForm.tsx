4b593f489db087e7347a13460748a171b818469e
david3010M <hvaldiviezos@unprg.edu.pe>
2026-09-07 16:28:28 -0500

    Update AdjustmentRequestForm.tsx

--- src/features/ap/comercial/solicitudes-cotizaciones/ajustes-margen/components/AdjustmentRequestForm.tsx
+++ src/features/ap/comercial/solicitudes-cotizaciones/ajustes-margen/components/AdjustmentRequestForm.tsx
@@ -14,7 +14,6 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import { NumberFormat } from "@/shared/components/NumberFormat";
 import { ConfirmationDialog } from "@/shared/components/ConfirmationDialog";
