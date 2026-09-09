b9cd086fc2765a80f9f25af6f06ad3a5a56027df
david3010M <hvaldiviezos@unprg.edu.pe>
2026-09-09 16:39:37 -0500

    Update VehicleColumns.tsx

--- src/features/ap/comercial/vehiculos/components/VehicleColumns.tsx
+++ src/features/ap/comercial/vehiculos/components/VehicleColumns.tsx
@@ -14,7 +14,6 @@ import {
   MODELS_VN,
   MODELS_VN_POSTVENTA,
 } from "@/features/ap/configuraciones/vehiculos/modelos-vn/lib/modelsVn.constanst";
-import { VEHICLE_STATUS_ID } from "@/features/ap/configuraciones/vehiculos/estados-vehiculo/lib/vehicleStatus.constants";
 
 export type VehicleColumns = ColumnDef<VehicleResource>;
 
@@ -139,7 +138,7 @@ export const vehicleColumns = ({
         warehouse_id,
         warehouse_name,
         model,
-        ap_vehicle_status_id,
+        warehouse_is_received,
       } = row.original;
       // eslint-disable-next-line react-hooks/rules-of-hooks
       const router = useNavigate();
@@ -167,16 +166,15 @@ export const vehicleColumns = ({
           )}
 
           {/* Change Location */}
-          {permissions.canChangeLocation &&
-            ap_vehicle_status_id === VEHICLE_STATUS_ID.VEHICULO_EN_TRANSITO && (
-              <ChangeLocationModal
-                vehicleId={id}
-                vehicleVin={vin}
-                apClassArticleId={row.original.model.class_id}
-                currentWarehouseId={warehouse_id}
-                currentWarehouseName={warehouse_name}
-              />
-            )}
+          {permissions.canChangeLocation && warehouse_is_received == false && (
+            <ChangeLocationModal
+              vehicleId={id}
+              vehicleVin={vin}
+              apClassArticleId={row.original.model.class_id}
+              currentWarehouseId={warehouse_id}
+              currentWarehouseName={warehouse_name}
+            />
+          )}
 
           {/* Work Order History */}
           {permissions.canMaintenance && (
