3207e92311e73d59f68f05dea965d8a13ca8beaa
david3010M <hvaldiviezos@unprg.edu.pe>
2026-09-10 18:35:28 -0500

    auth

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -78,7 +78,7 @@ export function Login({ className, ...props }: React.ComponentProps<"div">) {
             >
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center text-center gap-2">
-                  <div className="relative h-16 aspect-[3]">
+                  <div className="relative h-12 aspect-[3]">
                     <img
                       src={CONSTANTS.LOGO}
                       alt="Image"
