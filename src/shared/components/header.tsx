d3bb7ddb39d217b85dd5fbf320dd324c62e55e3c
david3010M <hvaldiviezos@unprg.edu.pe>
2026-09-10 18:34:23 -0500

    se sube

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -14,7 +14,7 @@ export const LogoLink = ({ theme = "light" }: { theme?: string }) => (
     <img
       src={theme === "dark" ? CONSTANTS.LOGO_WHITE : CONSTANTS.LOGO}
       alt="Logo"
-      className="max-h-12 w-auto lg:h-10"
+      className="max-h-10 w-auto lg:h-8"
     />
   </Link>
 );
